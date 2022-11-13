494a5c8027695ba3c2e053fa0399acfc2afeaaa4
devnocto <devnocto@proton.me>
2022-11-13 17:10:42 -0400

    refactor: move primary button to component

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,23 +1,13 @@
-import { Dialog, Transition } from "@headlessui/react";
 import Head from "next/head";
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import BtnPrimary from "../components/BtnPrimary";
 import Game from "../components/Game";
-import { PongError } from "../interfaces";
 
 export default function Home() {
-  // Game screen
   const [start, setStart] = useState(false);
 
-  // Modal
-  const [isOpen, setIsOpen] = useState(false);
-  const [modalError, setModalError] = useState(PongError.None);
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
   return (
-    <div>
+    <>
       <Head>
         <title>Pong UI</title>
         <meta name="description" content="Play pong game online" />
@@ -32,71 +22,9 @@ export default function Home() {
           <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
             Play pong online.
           </p>
-          {!start ? (
-            <div>
-              <button
-                onClick={() => setStart(true)}
-                className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
-              >
-                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                  Start game
-                </span>
-              </button>
-            </div>
-          ) : (
-            <Game setModalError={setModalError} />
-          )}
+          {!start ? <BtnPrimary label="Start game" onClick={() => setStart(true)} /> : <Game />}
         </div>
       </main>
-
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
-          <Transition.Child
-            as={Fragment}
-            enter="ease-out duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="ease-in duration-200"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <div className="fixed inset-0 bg-black bg-opacity-25" />
-          </Transition.Child>
-
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white dark:bg-gray-900 p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900 dark:text-white">
-                    Error
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">{modalError}</p>
-                  </div>
-
-                  <div className="mt-4">
-                    <button
-                      type="button"
-                      className="text-white bg-cyan-700 hover:bg-cyan-800 focus:ring-4 focus:ring-cyan-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-cyan-600 dark:hover:bg-cyan-700 focus:outline-none dark:focus:ring-cyan-800"
-                      onClick={closeModal}
-                    >
-                      Continue
-                    </button>
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </div>
-        </Dialog>
-      </Transition>
-    </div>
+    </>
   );
 }
