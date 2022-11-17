ef7f3a3ae3b68060027af230d4cb2e84027cedf5
devnocto <devnocto@proton.me>
2022-11-16 23:36:42 -0400

    refactor: remove previous game class files

--- apps/web/src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-import { QueryClient } from "@tanstack/react-query";
-
-export const fetchFromCache = <T>(queryKey: string[], queryClient: QueryClient) => {
-  const data = queryClient.getQueryData(queryKey) as T | undefined;
-  if (data === undefined) {
-    throw new TypeError("data is undefined");
-  }
-  return data;
-};
